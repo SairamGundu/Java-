260601cb6293e897737458ef2d70c6a7755ff2f8
SairamGundu <165833309+SairamGundu@users.noreply.github.com>
2024-04-25 23:19:41 +0530

    Add files via upload

---
+++ Larger of two numbers/GreaterTwo.java
@@ -0,0 +1,16 @@
+//Write a program to find The larger of two numbers 
+
+//if condition
+class GreaterTwo {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        if (a > b) {
+            System.out.println("a is greater");
+
+        } else {
+            System.out.println("b is greater");
+        }
+
+    }
+
+}
