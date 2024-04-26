8f41b82f44e8f4c4c5b553a3898cbd9ee14ac1f8
SairamGundu <165833309+SairamGundu@users.noreply.github.com>
2024-04-26 23:18:37 +0530

    Add files via upload

---
+++ Even or Odd/EvenOdd.java
@@ -0,0 +1,14 @@
+//Write a program to find Even or odd numbers
+//90/2=45//quotient
+//90%2=0//remainder
+// if else condition
+public class EvenOdd {
+    public static void main(String[] args) {
+        int no = 80;
+        if (no % 2 == 0) {
+            System.out.println("Number is even");
+        } else {
+            System.out.println("Number is odd");
+        }
+    }
+}
